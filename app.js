3185bc8571792a1937a77b04ea0270738302ab8e
alex2022cci <alex2022cci@outlook.fr>
2023-04-23 21:37:15 +0200

    1st commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const items = document.querySelectorAll('img'); /*place toutes less images dans un tableau */
+const nbSlide = items.length;
+const suivant = document.querySelector('.right');
+const precedent = document.querySelector('.left');
+let count = 0;
+
+function SlideSuivante()
+{
+    items[count].classList.remove('active'); /*on retire la classe active de l'image courante*/
+    if(count < nbSlide - 1)                     /*on vérififie qu'il reste des images (count < max images)*/
+    {
+        count++;
+    }
+    else
+    {
+        count = 0;                              /*plus d'images ... on quitte la boucle*/
+    }
+    items[count].classList.add('active');  /*le nouvel item/image obtient la classe active */
+    console.log('count : ' + count);
+
+}
+
+suivant.addEventListener('click', SlideSuivante); /*on écoute le click sur le bouton suivant*/
+
+function SlidePrecedente()  /*code pour la flèche de gauche*/
+{
+    items[count].classList.remove('active');
+    if(count > 0)                                /*on vérififie qu'il reste des images (count > min images)*/
+    {
+        count--;
+    }
+    else
+    {
+        count = nbSlide - 1;
+    }
+    items[count].classList.add('active');
+    console.log('count : ' + count);
+}
+
+precedent.addEventListener('click', SlidePrecedente); /*on écoute le click sur le bouton précédent*/
